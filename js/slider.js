2c0e1e553a005b9c37ffd5bcd8f5b576dd831baf
Rostiquell <57566539+Rostiquell@users.noreply.github.com>
2020-12-08 18:45:37 +0200

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$('#slider-testimonials').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dotsClass: "my-dots",
+  prevArrow: $('.prev'),
+  nextArrow: $('.next')
+});
